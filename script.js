0af4269d5d029da877f4116d1ee4b6f683a77d1d
Jon <71684267+Sargismanthe2nd@users.noreply.github.com>
2023-12-03 21:16:05 -0800

    Added prompts

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 // Assignment code here
 function generatePassword() {
-  var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz'
-  var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  var numerals = '1234567890'
-  var symbols = '!@#$%&_'
-  var allCharacters = lowercaseCharacters + uppercaseCharacters + numerals + symbols;
-  var passwordLength = 10;
+  //Character lists//
+  var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numerals = '1234567890';
+  var symbols = '!@#$%&_';
+
+  // Password length prompt//
+  var passwordLength = prompt("Password Length:(between 8 and 128 characters):");
+  passwordLength = parseInt(passwordLength);
+
+  // Validate password length
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    passwordLength = prompt("Password length:      (between 8 and 128 characters):");
+    passwordLength = parseInt(passwordLength);
+  }
+
+  // Prompt for character types
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumerals = confirm("Include numerals?");
+  var includeSymbols = confirm("Include symbols?");
+
+  
+  // Generate the password
   var generatedPassword = '';
   for (var i = 0; i < passwordLength; i++) {
     var randomIndex = Math.floor(Math.random() * allCharacters.length);
     generatedPassword += allCharacters.charAt(randomIndex);
-  
   }
+
   return generatedPassword;
 }
 
@@ -24,7 +43,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
